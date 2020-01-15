dbbf4129e5f6d71ce1de97a4bd26170996284383
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2020-01-15 17:47:08 +0100

    WGDPIPE-21 Proteger el acceso administrador

--- sources/views/mainMenu.js
+++ sources/views/mainMenu.js
@@ -4,42 +4,46 @@ import { usuarioService } from "../services/usuario_service";
 export default class MainMenu extends JetView {
     config() {
         const translate = this.app.getService("locale")._;
+        var usu = usuarioService.getUsuarioCookie();
+        console.log('Usuario', usu);
+        let dataMenu = [];
+        dataMenu.push(                {
+            id: "inicio", icon: "mdi mdi-home", value: translate("INICIO")
+        });
+        if (usu.esAdministrador) {
+            dataMenu.push(                {
+                icon: "mdi mdi-switch", open: true, value: translate("ADMINISTRACIÓN"),
+                data: [
+                    { id: "parametros", value: translate("Parámetros"), icon: "mdi mdi-altimeter", details: "Parámetros de la aplicación", },
+                    { id: "gruposUsuarios", value: translate("Grupos de visualización"), icon: "mdi mdi-account-group", details: "Grupos de usuarios" },
+                    { id: "usuarios", value: translate("Usuarios"), icon: "mdi mdi-account", details: "Usuarios" },
+                    { id: "paises", value: translate("Paises"), icon: "mdi mdi-flag", details: "Paises" },
+                    { id: "empresas", value: translate("Empresas"), icon: "mdi mdi-cube", details: "Empresas" },
+                    { id: "unidadesNegocio", value: translate("Unidades de negocio"), icon: "mdi mdi-cube-scan", details: "Unidades de negocio" },
+                    { id: "areas", value: translate("Areas"), icon: "mdi mdi-cube-outline", details: "Areas" },
+                    { id: "servicios", value: translate("Servicios"), icon: "mdi mdi-cube-unfolded", details: "Servicios" },
+                    { id: "fasesOferta", value: translate("Fases de oferta"), icon: "mdi mdi-filter-variant", details: "Fases de oferta" },
+                    { id: "tiposOportunidad", value: translate("Tipos de oportunidad"), icon: "mdi mdi-shape", details: "Tipos de oportunidad" },
+                    { id: "tiposContrato", value: translate("Tipos de contrato"), icon: "mdi mdi-shape-outline", details: "Tipos de contrato" },
+                    { id: "estados", value: translate("Estados"), icon: "mdi mdi-thumbs-up-down", details: "Estados" },
+                    { id: "razonesPerdida", value: translate("Razon perdida"), icon: "mdi mdi-thumb-down", details: "Razon perdida" },
+                    { id: "divisas", value: translate("Divisas"), icon: "mdi mdi-currency-eur", details: "Divisas" },
+                    { id: "ubicaciones", value: translate("Ubicaciones"), icon: "mdi  mdi-map-marker-outline", details: "Ubicaciones" }
+
+                ]
+            });
+        }
+        dataMenu.push({
+            icon: "mdi mdi-buffer", open: true, value: translate("GESTIÓN"),
+            data: [
+                { id: "ofertas", value: translate("Ofertas"), icon: "mdi mdi-arrow-decision-outline", details: "Ofertas" }
+            ]
+        });
         var mainMenu = {
             view: "sidebar",
             id: "main:menu",
             activeTitle: true, select: true,
-            data: [
-                {
-                    id: "inicio", icon: "mdi mdi-home", value: translate("INICIO")
-                },
-                {
-                    icon: "mdi mdi-switch", open: true, value: translate("ADMINISTRACIÓN"),
-                    data: [
-                        { id: "parametros", value: translate("Parámetros"), icon: "mdi mdi-altimeter", details: "Parámetros de la aplicación", },
-                        { id: "gruposUsuarios", value: translate("Grupos de visualización"), icon: "mdi mdi-account-group", details: "Grupos de usuarios" },
-                        { id: "usuarios", value: translate("Usuarios"), icon: "mdi mdi-account", details: "Usuarios" },
-                        { id: "paises", value: translate("Paises"), icon: "mdi mdi-flag", details: "Paises" },
-                        { id: "empresas", value: translate("Empresas"), icon: "mdi mdi-cube", details: "Empresas" },
-                        { id: "unidadesNegocio", value: translate("Unidades de negocio"), icon: "mdi mdi-cube-scan", details: "Unidades de negocio" },
-                        { id: "areas", value: translate("Areas"), icon: "mdi mdi-cube-outline", details: "Areas" },
-                        { id: "servicios", value: translate("Servicios"), icon: "mdi mdi-cube-unfolded", details: "Servicios" },
-                        { id: "fasesOferta", value: translate("Fases de oferta"), icon: "mdi mdi-filter-variant", details: "Fases de oferta" },
-                        { id: "tiposOportunidad", value: translate("Tipos de oportunidad"), icon: "mdi mdi-shape", details: "Tipos de oportunidad" },
-                        { id: "tiposContrato", value: translate("Tipos de contrato"), icon: "mdi mdi-shape-outline", details: "Tipos de contrato" },
-                        { id: "estados", value: translate("Estados"), icon: "mdi mdi-thumbs-up-down", details: "Estados" },
-                        { id: "razonesPerdida", value: translate("Razon perdida"), icon: "mdi mdi-thumb-down", details: "Razon perdida" },
-                        { id: "divisas", value: translate("Divisas"), icon: "mdi mdi-currency-eur", details: "Divisas" },
-                        { id: "ubicaciones", value: translate("Ubicaciones"), icon: "mdi  mdi-map-marker-outline", details: "Ubicaciones" }
-
-                    ]
-                },
-                {
-                    icon: "mdi mdi-buffer", open: true, value: translate("GESTIÓN"),
-                    data: [
-                        { id: "ofertas", value: translate("Ofertas"), icon: "mdi mdi-arrow-decision-outline", details: "Ofertas" }
-                    ]
-                }
-            ]
+            data: dataMenu
         };
         return mainMenu;
     }
