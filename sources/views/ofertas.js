c10129d1cd0ad5927b36173b6eb2cd7d7e802ba2
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2021-03-23 09:54:56 +0100

    WGDPIPE-100 Cambiar la posición del número de oferta en el pool

--- sources/views/ofertas.js
+++ sources/views/ofertas.js
@@ -225,6 +225,7 @@ export default class Ofertas extends JetView {
                 { id: "areaId", header: [translate("Area"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colAreas, width: 150 },
                 { id: "ubicacionId", header: [translate("Ubicacion"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colUbicaciones, width: 200 },
                 { id: "cliente", header: [translate("Cliente"), { content: "textFilter" }], sort: "string", editor: "text", width: 150 },
+                { id: "numeroOferta", header: [translate("Nr. Oferta"), { content: "textFilter" }], sort: "string", editor: "text", minWidth: 100 },
                 { id: "nombreCorto", header: [translate("Nombre"), { content: "textFilter" }], sort: "string", editor: "text", width: 350 },
                 { id: "estadoId", header: [translate("Estado"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colEstados, width: 100 },
                 { id: "importePresupuesto", adjust: true, header: [translate("Importe"), { content: "numberFilter" }], sort: "int", format: webix.i18n.priceFormat, css: { 'text-align': 'right' }, width: 100},
@@ -245,7 +246,6 @@ export default class Ofertas extends JetView {
                 { id: "faseOfertaId", header: [translate("Fase oferta"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colFasesOferta, width: 200 },
                 { id: "tipoOportunidadId", header: [translate("Tipo oportunidad"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colTiposOportunidad, width: 200 },
                 { id: "tipoContratoId", header: [translate("Tipo contrato"), { content: "selectFilter" }], sort: "string", editor: "combo", collection: colTiposContrato, width: 200 },
-                { id: "numeroOferta", header: [translate("Nr. Oferta"), { content: "textFilter" }], sort: "string", editor: "text", minWidth: 100 },
                 // { id: "ubicacion", header: [translate("Ubicación"), { content: "textFilter" }], sort: "string", editor: "text", width: 200 },
                 {
                     id: "fechaAdjudicacion", header: [{ text: translate("Fecha adjudicación"), css: { "text-align": "center" } }, { content: "textFilter" }],
